1cd79d00f72ff7f5da23bd18ca6dfdae47b8682c
Khaled Ali Ahmed <104729445+Engineered0@users.noreply.github.com>
2024-08-11 17:40:22 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import OCRReader from "./OCRReader";
+
+export default function App() {
+  return (
+    <main>
+      GHW MLH - AI/ML in JavaScript
+      <OCRReader />
+    </main>
+  );
+}
